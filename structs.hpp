006ecfd7eaf95983be3f5c447b288e2755f76344
vws6c <113268204+vws6c@users.noreply.github.com>
2026-09-15 23:13:09 +0300

    mydumbass made gap decimal cooked

--- structs.hpp
+++ structs.hpp
@@ -1,14 +1,14 @@
 struct RobloxExtraSpace
 {
-    unsigned char gap_00[24];       // 0
-    std::shared_ptr<Shared> shared; // 24
-    unsigned char gap_01[8];        // 40
-    uint64_t capabilities;          // 48
-    unsigned char gap_02[24];       // 56
-    std::weak_ptr<uintptr_t> script;// 80
-    unsigned char gap_03[8];        // 96
-    RBX::Identity identity;         // 104
-    RBX::Lua::Continuations *continuations; // 112
-    unsigned char gap_04[32];       // 120
-    std::weak_ptr<uintptr_t> Actor; // 152
+    unsigned char gap[0x18];                 // 0
+    std::shared_ptr<Shared> shared;          // 24
+    unsigned char gap[0x08];                 // 0
+    uint64_t capabilities;                   // 48
+    unsigned char gap[0x18];                 // 0
+    std::weak_ptr<uintptr_t> script;         // 80
+    unsigned char gap[0x08];                 // 0
+    RBX::Identity identity;                  // 104
+    RBX::Lua::Continuations *continuations;  // 112
+    unsigned char gap[0x20];                 // 0
+    std::weak_ptr<uintptr_t> Actor;          // 152
 };
