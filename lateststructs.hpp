18dca50eac8d496fd052af23518b5a994a70f212
vws6c <113268204+vws6c@users.noreply.github.com>
2026-09-16 22:54:32 +0300

    Fix 2 incorrect placments and confirm dump

--- lateststructs.hpp
+++ lateststructs.hpp
@@ -1,16 +1,17 @@
-// unchecked, unconfirmed, test dump, i will skid thorioums structs to confirm or smt if he dumps
+/// all confirmed thanks to thorioums dump except for continuations
 
 struct RobloxExtraSpace
 {
     unsigned char gap[0x18];                 // 0
     std::shared_ptr<Shared> shared;          // 24
-    unsigned char gap[0x08];                 // 0
-    uint64_t capabilities;                   // 48
-    unsigned char gap[0x18];                 // 0
+    unsigned char gap_0[0x08];               // 40
+    RBX::Identity identity;                  // 48
+    unsigned char gap_1[0x18];               // 56
     std::weak_ptr<uintptr_t> script;         // 80
-    unsigned char gap[0x08];                 // 0
-    RBX::Identity identity;                  // 104
+    unsigned char gap_2[0x10];               // 96
     RBX::Lua::Continuations *continuations;  // 112
-    unsigned char gap[0x20];                 // 0
+    unsigned char gap_3[0x18];               // 120
+    uint64_t capabilities;                   // 144
     std::weak_ptr<uintptr_t> Actor;          // 152
+    unsigned char gap_4[0x10];               // 168
 };
