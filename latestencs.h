c1e698037bc8292b83857b0ca664493f491bcc0d
vws6c <113268204+vws6c@users.noreply.github.com>
2026-09-21 19:42:43 +0300

    Add latestencs.h with encoding definitions

---
+++ latestencs.h
@@ -0,0 +1,12 @@
+#define CLOSURE_CONT_ENC       VMValue2
+#define PROTO_LINEINFO_ENC     VMValue2
+#define PROTO_ABSLINEINFO_ENC  VMValue2
+#define PROTO_LOCVARS_ENC      VMValue4
+#define PROTO_UPVALUES_ENC     VMValue3
+#define PROTO_SOURCE_ENC       VMValue3
+#define PROTO_DEBUGINSN_ENC    VMValue4
+#define PROTO_TYPEINFO_ENC     VMValue2
+#define LSTATE_STACKSIZE_ENC   VMValue1
+#define CLOSURE_DEBUGNAME_ENC  VMValue4
+#define UDATA_META_ENC         VMValue3
+#define TSTRING_HASH_ENC       VMValue3
